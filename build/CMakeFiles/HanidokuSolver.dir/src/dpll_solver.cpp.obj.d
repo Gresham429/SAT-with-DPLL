959b79d70871e79c7e2de4993ad2a4b6dd062d3b
GRESHAME\15437 <1543732388@qq.com>
2023-09-05 22:48:17 +0800

    delete(build): clean the menu

--- build/CMakeFiles/HanidokuSolver.dir/src/dpll_solver.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/HanidokuSolver.dir/src/dpll_solver.cpp.obj: \
- D:\code\HanidokuSolver\src\dpll_solver.cpp
--- build/CMakeFiles/HanidokuSolver.dir/src/hanidoku_solver.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/HanidokuSolver.dir/src/hanidoku_solver.cpp.obj: \
- D:\code\HanidokuSolver\src\hanidoku_solver.cpp
